0f915661fca57aac71c65caea6066951f28bc60a
kattamuri susmitha <susmithakattamuri5@gmail.com>
2025-06-13 23:06:00 +0530

    these are inside client and server folder.

---
+++ index.js
@@ -0,0 +1,31 @@
+// server/index.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('message', (data) => {
+    io.emit('message', data); // Broadcast to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
